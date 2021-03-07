5e2f2bfc159a94efa8049d3967a02c5b0198e3a2
stir101 <476395@chrome.escg.ac.uk>
2021-03-07 23:06:08 +0000

    finished healthbar tutorial

---
+++ Assets/Kitty.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kitty : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public Health health;
+    
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        health.SetMaxHealth(maxHealth);
+    }
+
+    private void Update()
+    {
+        //replace this with remove over time x 1.x x being bigger or smaller per level
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        health.SetHealth(currentHealth);
+    }
+
+}
