b9293baf320d968e02d2db55178c310e117b0481
stir101 <476395@chrome.escg.ac.uk>
2021-03-10 20:37:49 +0000

    started adding the upgrade functions

---
+++ Assets/Money.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public double coins;
+
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Upgrades.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    public GameObject manageHolder;
+    public Money money;
+    
+
+
+    private void Start()
+    {
+        manageHolder = GameObject.FindGameObjectWithTag("Manager");
+        money = manageHolder.GetComponent<Money>();
+    }
+
+
+    public void UP1()
+    {
+        if (money.coins == 20)
+        {
+
+
+        }
+    }
+}
