ff7849533ff0d1e103dbf1c30e70b8c3478a3f2b
stir101 <476395@chrome.escg.ac.uk>
2021-03-21 19:17:53 +0000

    added health drain mechanic

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -5,30 +5,102 @@ using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public int maxHealth;
-    public int currentHealth;
-
-    public GameObject childHold;
-    public Transform fillHold;
     
+    
+
+    public float maxSize;
+    public float currentTimer;
+    public float waitTime;
+
+    public Transform scaled;
+
     public void Start()
     {
-        //Assigns the transform of the first child of the Game Object this script is attached to.
-        fillHold = this.gameObject.transform.GetChild(0);
+        scaled = transform.Find("HealthBar/FillHold");
 
-        //Assigns the first child of the first child of the Game Object this script is attached to.
-        //grandChild = this.gameObject.transform.GetChild(0).GetChild(0).gameObject;
+        //SetMaxHealth();
+
+        StartCoroutine(Scale());
+
+        switch (gameObject.tag)
+        {
+            case "K1":
+                currentTimer = 1.0f; 
+                break;
+            case "K2":
+                currentTimer = 5.0f;
+                break;
+            case "K3":
+                currentTimer = 100.0f;
+                break;
+            case "K4":
+                currentTimer = 1.0f;
+                break;
+            case "K5":
+                currentTimer = 1.0f;
+                break;
+            case "K6":
+                currentTimer = 1.0f;
+                break;
+            case "K7":
+                currentTimer = 1.0f;
+                break;
+            case "K8":
+                currentTimer = 1.0f;
+                break;
+            case "K9":
+                currentTimer = 1.0f;
+                break;
+            default:
+                Debug.Log("not sure what to do with this right now lol");
+                break;
+        }
     }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetMaxHealth();
+        }
+    }
+
+
+
 
     public void SetMaxHealth()
     {
-        //child scale x - 2.85
-        //min = 0
-        maxHealth = 100;
+        scaled.transform.localScale = new Vector3(2.9f, scaled.transform.localScale.y, scaled.transform.localScale.z);
     }
 
-    public void SetHealth()
+    IEnumerator Scale()
     {
-        
-    }
+        float timer = 0;
+
+        while (true)
+        {
+            // we scale all axis, so they will have the same value, 
+            // so we can work with a float instead of comparing vectors
+            while (maxSize > scaled.transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                scaled.transform.localScale += new Vector3(1 * Time.deltaTime * currentTimer, 0, 0);
+                yield return null;
+            }
+
+            // reset the timer
+            yield return new WaitForSeconds(waitTime);
+
+            timer = 0;
+            while (0 < scaled.transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                scaled.transform.localScale -= new Vector3(1 * Time.deltaTime * currentTimer, 0, 0);
+                yield return null;
+            }
+
+            timer = 0;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }    
 }
