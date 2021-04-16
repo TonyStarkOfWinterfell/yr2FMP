e217648042d58e58e128ead47f3891caeb777c50
stir101 <476395@chrome.escg.ac.uk>
2021-04-17 00:21:50 +0100

    added pop up tutorial system

---
+++ Assets/PopUpSys.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpSys : MonoBehaviour
+{
+    public GameObject popUpBox;
+    public Text popUpText;
+    public string popText;
+
+    public string currentOK;
+
+    public bool hasMerged;                      //potential problem with the object being disabled and trying to perform tasks
+                                               //fix by putting script onto seperate manager game object  (try and do this before calling methods from other scripts
+    public bool mergeOK;
+    public bool hasMoney;
+
+    public bool foodOK;
+    public bool isSick;
+
+    public bool mergeBreak;
+    public bool upgradeBreak;
+    public bool vetBreak;
+
+    public void Start()
+    {
+        currentOK = "START";
+        popText = "WELCOME TO\nPAWSITIVELY PANDORA";
+
+        hasMerged = false;
+
+        mergeOK = false;
+        hasMoney = false;
+
+        foodOK = false;
+        isSick = false;
+
+        mergeBreak = false;
+        upgradeBreak = false;
+        vetBreak = false;
+    }
+
+    public void Update()
+    {
+        //if bool true then > PopUp("");
+        //add this to events. once gotten then set this script thing to something.
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PopUp("it worked");
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            hasMerged = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            mergeOK = true;
+            hasMoney = true;
+        }*/
+
+        if (mergeBreak == false)
+        {
+            if (hasMerged == true)
+            {
+                PopUp("WELL DONE, YOU HAVE SOME\nLOCAL STRAYS! SUCH RASCALS!");
+                currentOK = "MERG1";
+
+                mergeBreak = true;
+            }
+        }
+
+        if (upgradeBreak == false)
+        {
+            if (mergeOK == true && hasMoney == true)
+            {
+                PopUp("YOU CAN UNLOCK AN UPGRADE ON THE\nRIGHT PANEL, THEY WILL SURELY HELP\nIN YOUR ADVENTURES");
+                currentOK = "UPGRADE";
+
+                upgradeBreak = true;
+            }
+        }
+
+        if (vetBreak == false)
+        {
+            if (foodOK == true && isSick == true)
+            {
+                PopUp("SICK CATS CAN'T BREED OR GENERATE\nINCOME! DRAG THE GREEN TINTED CATS TO\nTHE VET ON THE LEFT SIDE\n TO HEAL IT FOR £500!");
+                currentOK = "VET";
+
+                vetBreak = true;
+            }
+        }
+
+    }
+
+    public void PopUp(string text)
+    {
+        popUpBox.SetActive(true);
+        popUpText.text = text;
+        //potential animation addition
+    }
+
+    public void OK()
+    {
+        //need to add enter code once i find it
+        //can add you win here or last upgrade
+
+        switch (currentOK)
+        {
+            case "START":
+                PopUp("YOU HAVE RECENTLY FOUND YOUR\nPASSION AS A CAT BREEDER!\nIT'S TIME TO MAKE A FUR-TUNE");
+                currentOK = "INTRO1"; 
+                break;
+            case "INTRO1":
+                PopUp("START BY CLICKING ON THE\nPAW IN THE TOP LEFT CORNER\nTO ATTRACT SOME CATS!");
+                currentOK = "INTRO2";
+                break;
+            case "INTRO2":
+                popUpBox.SetActive(false);
+                break;
+         //=====================\\         
+            case "MERG1":
+                PopUp("NOW IT'S TIME FOR YOUR FIRST BREED!\nBY BREEDING TWO OF THE SAME\nCATS YOU IMPROVE THEIR CUTENESS");
+                currentOK = "MERG2";
+                break;
+            case "MERG2":
+                PopUp("CUTER CATS WILL EARN MORE\nMONEY BOTH PASSIVELY AND\nWHEN SOLD TO THE PET\nSHOP IN THE TOP LEFT");
+                currentOK = "MERG3";
+                break;
+            case "MERG3":
+                popUpBox.SetActive(false);
+                mergeOK = true;                
+                break;
+         //=====================\\     
+            case "UPGRADE":
+                PopUp("BELOW YOUR UPGRADES IS\nA PLACE TO BUY FOOD, EACH\nCOSTING £100. MAKE SURE TO\nFEED YOUR CATS IF\nTHEY GET LOW ON HEALTH!");
+                currentOK = "FOOD";
+                break;
+            case "FOOD":
+                popUpBox.SetActive(false);
+                foodOK = true;
+                break;               
+         //=====================\\     
+            case "VET":
+                popUpBox.SetActive(false);
+                break;
+            default:
+                PopUp("eRR0r so\nme\nwhe\nre");
+                break;
+        }
+    }
+}
