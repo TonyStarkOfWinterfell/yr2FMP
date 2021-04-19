4660e4cd40120fa93d22ca93fc4041fbcc6d932f
stir101 <476395@chrome.escg.ac.uk>
2021-04-19 19:45:51 +0100

    added random movement

---
+++ Assets/Patrol.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform moveSpot;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    
+    void Start()
+    {
+        startWaitTime = Random.Range(8f, 20f);
+        waitTime = startWaitTime;
+
+        moveSpot = GameObject.FindGameObjectWithTag("Move").transform;
+        minX = -2.8f;
+        maxX = 4.1f;
+        minY = -4.5f;
+        maxY = 3.25f;
+        speed = 1.2f;
+
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    }
+
+   
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);    
+        
+        if(Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
