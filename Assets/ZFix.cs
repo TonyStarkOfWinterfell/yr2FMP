5d865a96946f2a7925b9488d7815bb82f02cc7a5
stir101 <476395@chrome.escg.ac.uk>
2021-05-19 15:30:25 +0100

    added scroll arrows and ffinally fixed z position problem

---
+++ Assets/ZFix.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZFix : MonoBehaviour
+{
+    void Awake()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 50);        
+    }
+
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 50);
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 50);
+    }
+}
